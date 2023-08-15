b9eb6465edab7e6d3eca21f345d42ef4ceda00e8
Soura700 <108584756+Soura700@users.noreply.github.com>
2023-08-15 23:33:06 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const app = express();
-var connection = require("./connection")
-var registerAuth = require("./routes/auth")
 
-//Step 1:
-dotenv.config();
-// Step 2:
-app.use(express.json());
 
-//step 3:
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database: ', err);
-        return;
-    }
 
-    console.log('Connected to the database');
+// Set app port
+app.listen(4000, () => {
+    console.log("Server listening on port 4000");
 });
-PORT = 5000;
-app.use("/api/auth", registerAuth);
-
-console.log("hello");
-
-
-
-//step 5:
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))
+console.log("hello")
