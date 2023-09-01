6a1135134ebf38d4f12b3dd27446011a63c7c084
Soura700 <108584756+Soura700@users.noreply.github.com>
2023-09-01 21:01:12 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -159,7 +159,7 @@ function App() {
           element: <Home />,
         },
         {
-          path: "/profile",
+          path: "/profile/:userId",
           element: <Profile />,
         },
       ],
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Posts from '../../components/Posts/Posts';
 import styles from './home.module.css';
-// import Share from "../../components/Share/Share";
+import Stories from '../../components/Stories/Stories';
+import Share from "../../components/Share/Share";
 
 const Home = () => {
   return (
     <div className='home'>
+      {/* <Stories/> */}
+      {/* <Share/> */}
       <Posts/>
     </div>
   )
