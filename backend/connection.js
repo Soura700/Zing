0ab0e2fd10e3f49e99d12cfde1a818c4e7414542
Soura700 <108584756+Soura700@users.noreply.github.com>
2024-02-20 12:13:29 +0530

    changes(20/2/2024)

--- backend/connection.js
+++ backend/connection.js
@@ -6,6 +6,14 @@ const connection = mysql.createConnection({
     user:"root",
     password: "root123",
     database:"socialmedia"
+    // host: "sql6.freemysqlhosting.net",
+    // user:"sql6684938",
+    // password: "UpuBjKez3X",
+    // database:"sql6684938"
+    // host: "sql207.infinityfree.com",
+    // user:"if0_36010304",
+    // password: "cnJlNk5hkJkQfV",
+    // database:"if0_36010304_socialmedia",
   });
 
 
