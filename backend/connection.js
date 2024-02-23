fbee4f45d89b133b3a9c70a77d66cf404efa79f5
Soura700 <108584756+Soura700@users.noreply.github.com>
2024-02-23 19:01:57 +0530

    cpnnection.js

--- backend/connection.js
+++ backend/connection.js
@@ -6,17 +6,23 @@ const connection = mysql.createConnection({
   // password: "root123",
   // database:"socialmedia"
 
-    host : "pgr.h.filess.io",
-    user : "zing_withouttax",
-    password : "ed055c82c681e804d823c4ec2f642b466cfbcdc3",
-    database : "zing_withouttax",
-    port:"3307",
+    // host : "pgr.h.filess.io",
+    // user : "zing_withouttax",
+    // password : "ed055c82c681e804d823c4ec2f642b466cfbcdc3",
+    // database : "zing_withouttax",
+    // port:"3307",
 
-    host: "3p7.h.filess.io",
-    user: "Zing_wasseenme",
-    password: "aba4fbd7bf4c8e9979353d5817480b241dfd5806",
-    database: "Zing_wasseenme",
-    port: "3307",
+    // host: "3p7.h.filess.io",
+    // user: "Zing_wasseenme",
+    // password: "aba4fbd7bf4c8e9979353d5817480b241dfd5806",
+    // database: "Zing_wasseenme",
+    // port: "3307",
+
+    user: "avnadmin",
+    password: "AVNS_btM1p0D9KHnIG7blQFq",
+    host: "mysql-2c37a9e7-samadritcse-c448.a.aivencloud.com",
+    database: "defaultdb",
+    port: "23390",
 
     
   });
@@ -26,12 +32,6 @@ const connection = mysql.createConnection({
   // database: "Zing_wasseenme",
   // port: "3307",
 
-  user: "avnadmin",
-  password: "AVNS_btM1p0D9KHnIG7blQFq",
-  host: "mysql-2c37a9e7-samadritcse-c448.a.aivencloud.com",
-  database: "defaultdb",
-  port: "23390",
-});
 
 
   module.exports=connection;
