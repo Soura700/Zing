9561de640d73ef0b888e5952975242f031c9cbdc
Soura Bose <108584756+Soura700@users.noreply.github.com>
2024-02-23 11:13:32 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -7,17 +7,19 @@ const connection = mysql.createConnection({
     // password: "root123",
     // database:"socialmedia"
 
-    host : "pgr.h.filess.io",
-    user : "zing_withouttax",
-    password : "ed055c82c681e804d823c4ec2f642b466cfbcdc3",
-    database : "zing_withouttax",
-    port:"3307",
+    // host : "pgr.h.filess.io",
+    // user : "zing_withouttax",
+    // password : "ed055c82c681e804d823c4ec2f642b466cfbcdc3",
+    // database : "zing_withouttax",
+    // port:"3307",
 
-    // host: "3p7.h.filess.io",
-    // user: "Zing_wasseenme",
-    // password: "aba4fbd7bf4c8e9979353d5817480b241dfd5806",
-    // database: "Zing_wasseenme",
-    // port: 3307,
+    host: "3p7.h.filess.io",
+    user: "Zing_wasseenme",
+    password: "aba4fbd7bf4c8e9979353d5817480b241dfd5806",
+    database: "Zing_wasseenme",
+    port: "3307",
+
+    
   });
 
 
